0752cde7af2ec9738acfea8e5164c8ddb22665cb
Ansh Shah <ansh.shah.1502@gmail.com>
2026-04-29 14:41:54 +0530

    Cleanup: Remove unused scripts and scratch files for deployment

--- scratch/check_maxfolio_dates.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-DATA_PATH = r"C:\Users\LENOVO\Desktop\Ocean Finvest\MOMENTUM_DB_2 copy\Momentum_Maxfolio.xlsx"
-df = pd.read_excel(DATA_PATH)
-df["Date"] = pd.to_datetime(df["Date"])
-print("Last 5 dates in Maxfolio:", df["Date"].unique()[-5:])
